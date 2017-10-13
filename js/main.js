92d13ee3fac742a91f60adbd052659850f75e132
Alisa Filatova <alicefill@bk.ru>
2017-10-14 02:24:45 +0300

    add adaptive

--- js/main.js
+++ js/main.js
@@ -1,17 +1,26 @@
-var enterBtn = document.querySelector('.user-operations__btn_enter');
-var registrationBtn = document.querySelector('.user-operations__btn_registration');
+var userBlockBtn = document.querySelector('.user-operations__btn');
+
 var card = document.querySelector('#card');
 var cardItems = document.querySelector('.card-items');
 var cardDeleteBtn = document.querySelectorAll('.card-item__btn-delete');
+
 var catalogBtn = document.querySelector('#catalog-btn');
 var catalog = document.querySelector('.catalog');
+
 var registrationWindow = document.querySelector('.modal-window_registration');
 var enterWindow = document.querySelector('.modal-window_enter');
+
 var overlay = document.querySelector('.overlay');
+
 var registrationActive = document.querySelector('#registration-active');
 var registrationDisabled = document.querySelector('#registration-disabled');
 var enterActive = document.querySelector('#enter-active');
 var enterDisabled = document.querySelector('#enter-disabled');
+
+var userIcon = document.querySelector('.user-block__icon_color');
+
+var searchField = document.querySelector('#search');
+var searchIcon = document.querySelector('.search__icon_color');
 
 catalogBtn.addEventListener('click', function(event) {
     event.preventDefault();
@@ -19,17 +28,27 @@ catalogBtn.addEventListener('click', function(event) {
     this.classList.add('main-nav__menu-item_link-active');
 });
 
-enterBtn.addEventListener('click', function(event) {
-    event.preventDefault();
-    enterWindow.classList.remove('hide');
-    this.classList.add('user-operations__btn_enter-active');
-    overlay.classList.remove('hide');
+catalogBtn.addEventListener('mouseover', function() {
+    catalog.classList.remove('hide');
+    this.classList.add('main-nav__menu-item_link-active');
 });
 
-registrationBtn.addEventListener('click', function(event) {
+catalog.addEventListener('mousemove', function() {
+    catalog.classList.remove('hide');
+    catalogBtn.classList.add('main-nav__menu-item_link-active');
+});
+
+catalog.addEventListener('mouseout', function() {
+    this.classList.add('hide');
+    catalogBtn.classList.remove('main-nav__menu-item_link-active');
+});
+
+userBlockBtn.addEventListener('click', function(event) {
     event.preventDefault();
-    registrationWindow.classList.remove('hide');
-    this.classList.add('user-operations__btn_enter-active');
+    enterWindow.classList.remove('hide');
+    this.classList.add('user-operations__btn-active');
+    userIcon.classList.add('user-block__icon_color-focus');
+
     overlay.classList.remove('hide');
 });
 
@@ -37,32 +56,50 @@ enterDisabled.addEventListener('click', function(event) {
     event.preventDefault();
     registrationWindow.classList.add('hide');
     enterWindow.classList.remove('hide');
-    this.classList.add('user-operations__btn_enter-active');
-    registrationDisabled.classList.add('user-operations__btn_enter-active');
-    registrationBtn.classList.remove('user-operations__btn_enter-active');
-    enterBtn.classList.add('user-operations__btn_enter-active');
+    this.classList.add('user-operations__btn-active');
+    registrationDisabled.classList.add('user-operations__btn-active');
 });
 
 registrationDisabled.addEventListener('click', function(event) {
     event.preventDefault();
     enterWindow.classList.add('hide');
     registrationWindow.classList.remove('hide');
-    this.classList.add('user-operations__btn_enter-active');
-    enterDisabled.classList.remove('user-operations__btn_enter-active');
-    enterBtn.classList.remove('user-operations__btn_enter-active');
-    registrationBtn.classList.add('user-operations__btn_enter-active');
+    this.classList.add('user-operations__btn_active');
+    enterDisabled.classList.remove('user-operations__btn-active');
 });
 
 card.addEventListener('click', function(event) {
     event.preventDefault();
     cardItems.classList.remove('hide');
     this.classList.add('card_active');
-    overlay.classList.remove('hide');
+});
+
+card.addEventListener('mouseover', function() {
+    cardItems.classList.remove('hide');
+    this.classList.add('card_active');
+});
+
+cardItems.addEventListener('mousemove', function() {
+    card.classList.add('card_active');
+    this.classList.remove('hide');
+});
+
+cardItems.addEventListener('mouseout', function() {
+    card.classList.remove('card_active');
+    this.classList.add('hide');
 });
 
 overlay.addEventListener('click', function() {
     registrationWindow.classList.add('hide');
-    cardItems.classList.add('hide');
     enterWindow.classList.add('hide');
+    userIcon.classList.remove('user-block__icon_color-focus');
     this.classList.add('hide');
+});
+
+searchField.addEventListener('focus', function() {
+    searchIcon.classList.add('search__icon_color-focus');
+});
+
+searchField.addEventListener('blur', function() {
+    searchIcon.classList.remove('search__icon_color-focus');
 });
